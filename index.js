ad8b6bc1c9d23d74ff3fa48d2b9ed5a964a008bc
junseuk <junseuk86@gmail.com>
2022-03-05 00:17:07 +0900

    added sound effects

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const score = document.querySelector('#score');
 const score2 = document.querySelector('#score2');
 const startGameBtn = document.querySelector('#start-game-btn');
 const modal = document.querySelector('#modal');
-
+const upgradeModal = document.querySelector('#upgrade-modal');
+const upgradeBtn = document.querySelector("#upgrade-btn");
+const buttonAudio = new Audio('button.mp3');
+const upgradeAudio = new Audio('upgrade.mp3');
+const backgroundAudio = new Audio('background.mp3');
+buttonAudio.volume = 0.3;
+upgradeAudio.volume = 0.3;
+backgroundAudio.volume = 0.8;
 class Player {
   constructor(x, y , radius, color) {
     this.x = x;
@@ -108,34 +115,33 @@ class Particle {
   }
 }
 
-let spawnInterval = 5000;
-let levelParam = 0.8;
-let minSpawnInterval = 1000;
+function spawning() {
+  console.log("hi")
+  //Max size = 30, Min size = 4
+  const radius = Math.random() * (30 - 4) + 4;
+  let x;
+  let y;
+  if (Math.random() < 0.5) {
+    x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+    y = Math.random() * canvas.height;
+  }
+  else {
+    x = Math.random() * canvas.width;
+    y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+  }
+  const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+  const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  enemies.push(new Enemy(x, y, radius, color, velocity));
+  setTimeout(spawning, spawnInterval);
+}
+
 function spawnEnemy() {
-  setTimeout(function spawning() {
-    console.log("inside interval ");
-    console.log("level: " + spawnInterval);
-    //Max size = 30, Min size = 4
-    const radius = Math.random() * (30 - 4) + 4;
-    let x;
-    let y;
-    if (Math.random() < 0.5) {
-      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
-      y = Math.random() * canvas.height;
-    }
-    else {
-      x = Math.random() * canvas.width;
-      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
-    }
-    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
-    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
-    const velocity = {
-      x: Math.cos(angle),
-      y: Math.sin(angle)
-    }
-    enemies.push(new Enemy(x, y, radius, color, velocity));
-    setTimeout(spawning, spawnInterval);
-  }, spawnInterval)
+  console.log("spawning enemy");
+  setTimeout(spawning, spawnInterval)
 }
 
 let player = new  Player(x, y, 15, 'white');
@@ -144,7 +150,11 @@ let enemies = [];
 let particles = [];
 let _score = 0;
 let upgrade = false;
-let numOfKill = 0;
+let upgradeCount = 0;
+let bossCount = 0;
+let spawnInterval = 5000;
+let minSpawnInterval = 1000;
+const levelParam = 0.8;
 
 function init() {
   player = new  Player(x, y, 15, 'white');
@@ -153,6 +163,9 @@ function init() {
   particles = [];
   spawnInterval = 5000;
   enemySpeed = 0.5;
+  bossCount = 0;
+  upgradeCount = 0;
+  upgrade = false;
   _score = 0;
   score.innerHTML = _score;
   score2.innerHTML = _score;
@@ -160,102 +173,100 @@ function init() {
 
 let animationId;
 function animate() {
-  console.log("inside animate");
-  if (numOfKill != 0 && numOfKill % 5 == 0) {
-    console.log("u killed:" + numOfKill);
-    numOfKill = 0;
-    console.log("u killed 2:", numOfKill);
+  if (upgradeCount != 0 && upgradeCount % 2 == 0) {
+    console.log("upgrade init");
+    upgradeCount = 0;
+    bossCount++;
     upgrade = true;
   }
   if (upgrade) {
-    setTimeout(() => {
-      console.log("please choose");
-      upgrade = false;
-      animationId = requestAnimationFrame(animate);
-    },2000)
+    console.log("upgrading");
+    cancelAnimationFrame(animationId);
+    upgradeModal.style.display = 'flex';
   }
-  if (!upgrade) {
+  else {
     animationId = requestAnimationFrame(animate);
-  }
-  c.fillStyle = 'rgba(0, 0, 0, 0.1)';
-  c.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
-  projectiles.forEach((projectile, index) => {
-    projectile.update();
-    //remove projectile if it goes off screen
-    if (projectile.x + projectile.radius < 0 ||
-        projectile.x - projectile.radius > canvas.width ||
-        projectile.y + projectile.radius < 0 ||
-        projectile.y - projectile.radius > canvas.height) {
-      setTimeout(() => {
-        projectiles.splice(index, 1);
-      }, 0)
-    }
-  });
-
-  enemies.forEach((enemy, index) => {
-    enemy.update();
-    const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
-    if (distance - enemy.radius - player.radius < 0) {
-      cancelAnimationFrame(animationId);
-      modal.style.display = 'flex';
-      score2.innerHTML = _score;
-    }
-    projectiles.forEach((projectile, projectileIndex) => {
-      const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-      if (distance - enemy.radius - projectile.radius < 0) {
-        //when projectile hits enemy
-        //create explosions
-        for (let i = 0; i < enemy.radius * 2; i++) {
-          particles.push(new Particle(
-            projectile.x, 
-            projectile.y,
-            Math.random() * 2, 
-            enemy.color, 
-            {
-              x: (Math.random() - 0.5) * (Math.random() * 8), 
-              y: (Math.random() - 0.5) * (Math.random() * 8)
-            }
-          ));
-        }
-        if (enemy.radius - 10 > 10) {
-          //increase score
-          _score += 10;
-          score.innerHTML = _score;
-          gsap.to(enemy, {
-            radius: enemy.radius - 10
-          });
-          setTimeout(() => {
-            projectiles.splice(projectileIndex, 1);
-          }, 0)
-        }
-        else {
-          //increase score with bonus
-          _score += 25;
-          score.innerHTML = _score;
-          numOfKill++;
-          console.log("kill: " + numOfKill);
-          //increase level
-          if (spawnInterval > minSpawnInterval) {
-            spawnInterval *= levelParam;
-            enemySpeed += 0.05;
-          }
-          setTimeout(()=> {
-            enemies.splice(index, 1);
-            projectiles.splice(projectileIndex, 1);
-          }, 0)
-        }
-      };
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach((projectile, index) => {
+      projectile.update();
+      //remove projectile if it goes off screen
+      if (projectile.x + projectile.radius < 0 ||
+          projectile.x - projectile.radius > canvas.width ||
+          projectile.y + projectile.radius < 0 ||
+          projectile.y - projectile.radius > canvas.height) {
+        setTimeout(() => {
+          projectiles.splice(index, 1);
+        }, 0)
+      }
     });
-  });
-
-  particles.forEach((particle, index) => {
-    if (particle.alpha <= 0) {
-      particles.splice(index, 1);
-    } else {
-      particle.update();
-    }
-  });
+  
+    enemies.forEach((enemy, index) => {
+      enemy.update();
+      const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+      if (distance - enemy.radius - player.radius < 0) {
+        cancelAnimationFrame(animationId);
+        modal.style.display = 'flex';
+        score2.innerHTML = _score;
+      }
+      projectiles.forEach((projectile, projectileIndex) => {
+        const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+        if (distance - enemy.radius - projectile.radius < 0) {
+          //when projectile hits enemy
+          //create explosions
+          for (let i = 0; i < enemy.radius * 2; i++) {
+            particles.push(new Particle(
+              projectile.x, 
+              projectile.y,
+              Math.random() * 2, 
+              enemy.color, 
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 8), 
+                y: (Math.random() - 0.5) * (Math.random() * 8)
+              }
+            ));
+          }
+          if (enemy.radius - 10 > 10) {
+            //increase score
+            _score += 10;
+            score.innerHTML = _score;
+            gsap.to(enemy, {
+              radius: enemy.radius - 10
+            });
+            setTimeout(() => {
+              projectiles.splice(projectileIndex, 1);
+            }, 0)
+          }
+          else {
+            //increase score with bonus
+            _score += 25;
+            score.innerHTML = _score;
+            upgradeCount++;
+            console.log("kill: " + upgradeCount);
+            //increase level
+            if (spawnInterval > minSpawnInterval) {
+              spawnInterval *= levelParam;
+              enemySpeed += 0.05;
+              console.log(enemySpeed);
+            }
+            setTimeout(()=> {
+              enemies.splice(index, 1);
+              projectiles.splice(projectileIndex, 1);
+            }, 0)
+          }
+        };
+      });
+    });
+  
+    particles.forEach((particle, index) => {
+      if (particle.alpha <= 0) {
+        particles.splice(index, 1);
+      } else {
+        particle.update();
+      }
+    });
+  }
 };
 
 addEventListener('click', (e) => {
@@ -268,8 +279,17 @@ addEventListener('click', (e) => {
 })
 
 startGameBtn.addEventListener('click', () => {
+  backgroundAudio.play();
+  buttonAudio.play();
   init();
   modal.style.display = 'none';
   animate();
   spawnEnemy();
+});
+
+upgradeBtn.addEventListener('click', () => {
+  upgradeAudio.play();
+  upgrade = false;
+  upgradeModal.style.display = 'none';
+  animate();
 });
